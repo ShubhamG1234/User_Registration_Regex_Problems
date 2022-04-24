fa49f60089eced927d7f1bbbca43b7748ee0e153
ShubhamG1234 <shubhamgole28031996@gmail.com>
2022-04-24 21:08:42 +0530

    initial commit

--- User_Registration_Regex_Problems/User_Registration_Regex_Problems/Program.cs
+++ User_Registration_Regex_Problems/User_Registration_Regex_Problems/Program.cs
@@ -7,15 +7,17 @@ namespace UserRegistration
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Regex User Registration Problem");
-            Console.WriteLine("Enter EmailId");
 
+            Console.WriteLine("Enter the Mobile Number");
             string inStr = Console.ReadLine();
-            string firsName = @"^[A-Z]{1}[a-zA-Z]{2,9}$";
-            string lastName = @"^[A-Z]{1}[a-zA-Z]{2,9}$";
-            string emailId = @"^[a-zA-Z0-9]+[._+-]{0,1}[a-zA-Z0-9]@[a-zA-Z0-9]{1,10}.[a-zA-Z]{2,10}[.][a-zA-Z]"; //creating pattern for emailid
 
-            Regex regex = new Regex(emailId);
+
+            string firstName = @"^[A-Z]{1}[a-zA-Z]{2,9}$";
+            string lastName = @"^[A-Z]{1}[a-zA-Z]{2,9}$";
+            string emailId = @"^[a-zA-Z0-9]+[._+-]{0,1}[a-zA-Z0-9]@[a-zA-Z0-9]{1,10}.[a-zA-Z]{2,10}[.][a-zA-Z]";
+            string mobileNum = @"^[0-9]{2}\s+[6-9]{1}[0-9]{9}$";
+            //creating pattern for emailid, mobile number
+            Regex regex = new Regex(mobileNum);
             Console.WriteLine(regex.IsMatch(inStr));
         }
     }
