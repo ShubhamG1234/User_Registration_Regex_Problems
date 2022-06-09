e0726503380684371485b5fc13f940dc8c3d9771
ShubhamG1234 <shubhamgole28031996@gmail.com>
2022-06-09 09:46:22 +0530

    Adding Pin code prob

---
+++ RegexDemo/Pattern.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexDemo
+{
+    public class Pattern
+    {
+        public string regex_pattern = "^[0-9]{1}[0-9]{2}[' ']*[0-9]{2}[0-9]{1}$";
+        public bool validatePinCode(string pincode)
+        {
+            return Regex.IsMatch(pincode, regex_pattern);
+        }
+    }
+}
---
+++ RegexDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace RegexDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Pattern pattern = new Pattern();
+
+            Console.WriteLine("Enter the Pattern");
+            string reg = Console.ReadLine();
+
+            bool val = pattern.validatePinCode(reg);
+
+            if (val)
+                Console.WriteLine("Pattern Matched");
+            else
+                Console.WriteLine("Pattern Not Matched") ;
+        }
+    }
+}
